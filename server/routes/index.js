992009d11edc996d50da197fe28cc21713e1186a
mariainv <mariaagnese.invernizzi@studenti.unimi.it>
2022-07-25 17:50:09 +0000

    template

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' , test:'ciao',number:45, username:'nome_utente_random', link:'/test'});
+});
+//aggiungo una route per far riferimento alla nuova pagina test.pug
+router.get('/test', function(req, res, next) {
+  res.render('test');
 });
 
 module.exports = router;
